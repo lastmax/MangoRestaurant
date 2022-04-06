6f3a2eb6312a1978be455befff5b8636f61ab266
lastmax <lastmax.job@gmail.com>
2022-04-06 22:12:42 +0300

    add ProductAPI: DbContext, models, nuget(AutoMapper, Swashbuckle, JwtBearer, EntityFrameworkCore(SqlServer, Tools))

---
+++ MangoRestaurant/Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Mango.Services.ProductAPI.Models.Dto;
+
+namespace Mango.Services.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDTO>> GetProducts();
+        Task<ProductDTO> GetProductById(int productId);
+        Task<ProductDTO> CreateUpdateProduct(ProductDTO productDto);
+        Task<bool> Delete(int productId);
+    }
+}
